39b5bff8a6e65320ba40a7090a1fa8e79f57d36a
Dimitre Ortt <dimiortt@gmail.com>
2022-06-09 18:57:27 -0300

    Created CustomFonts component

---
+++ src/view/components/CustomFonts/CustomFont.tsx
@@ -0,0 +1,32 @@
+import { Divider, Typography } from "@mui/material";
+import { Box } from "@mui/system";
+import React, { FunctionComponent } from "react";
+
+export type FontProps = {
+  children?: React.ReactNode;
+  center?: boolean;
+  font?: string;
+  size?: number;
+  color?: string;
+};
+
+export const CustomTypography: FunctionComponent<FontProps> = ({
+  children,
+  center,
+  font,
+  size,
+  ...rest
+}) => {
+  const sx: any = { ...rest, width: "100%" };
+  if (font) sx.fontFamily = font;
+  if (size) sx.fontSize = size;
+  return (
+    <Typography
+      component="span"
+      textAlign={center ? "center" : "start"}
+      sx={sx}
+    >
+      {children}
+    </Typography>
+  );
+};
